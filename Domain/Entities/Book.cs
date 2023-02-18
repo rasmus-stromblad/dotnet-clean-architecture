31631d0ea1c286eb530c2d6bbf5285b9d499c0f4
Rasmus <rasmus.stromblad@hotmail.com>
2023-02-18 15:05:56 +0100

    Added existing files

---
+++ Domain/Entities/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Author { get; set; }
+
+        public int Rating { get; set; }
+    }
+}
---
+++ Infrastructure/Repositories/BookRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Interfaces;
+
+namespace Infrastructure.Repositories
+{
+    public class BookRepository : IBookRepository
+    {
+        public static List<Domain.Entities.Book> books = new List<Domain.Entities.Book>
+        {
+            new Domain.Entities.Book { Id = 1, Name = "The Saturnian", Author = "Bob Bobson", Rating = 4 },
+            new Domain.Entities.Book { Id = 2, Name = "No Jobs : A biography", Author = "Steven Stevenson", Rating = 5 },
+            new Domain.Entities.Book { Id = 3, Name = "Larry Kotter", Author = "Aron Busk", Rating = 2 }
+        };
+
+        public List<Domain.Entities.Book> GetAll()
+        {
+            return books;
+        }
+    }
+}
